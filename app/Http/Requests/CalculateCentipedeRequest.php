edde41e1234241ea8a9640b58cbb527898645d73
yoichi-kaneko <kaneko@webstudio-wanderlust.voyage>
2023-06-19 20:53:41 +0900

    バリデーションの上限や下限の修正

--- app/Http/Requests/CalculateCentipedeRequest.php
+++ app/Http/Requests/CalculateCentipedeRequest.php
@@ -24,11 +24,11 @@ class CalculateCentipedeRequest extends FormRequest
     public function rules()
     {
         return [
-            'patterns.*.base_numerator' => 'required|integer|min:100|max:700',
+            'patterns.*.base_numerator' => 'required|integer|min:1|max:2000',
             'patterns.*.numerator_exp_1' => 'required|integer|min:1|max:5',
             'patterns.*.numerator_exp_2' => 'required|integer|min:1|max:5',
             'patterns.*.denominator_exp' => 'required|integer|min:0|max:12',
-            'max_step' => 'required|integer|min:10|max:200',
+            'max_step' => 'required|integer|min:1|max:200',
         ];
     }
 
