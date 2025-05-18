754b689ec1936d36861e03c986767dabcd77c86f
yoichi-kaneko <kaneko@webstudio-wanderlust.voyage>
2025-05-18 15:23:33 +0900

    ナッシュ均衡の描画処理を実装中

--- app/Calculations/Nash.php
+++ app/Calculations/Nash.php
@@ -28,9 +28,16 @@ class Nash
         $alpha_y = Fraction::fromString($alpha_2['numerator'] . '/' .  $alpha_2['denominator']);
         $beta_x = Fraction::fromString($beta_1['numerator'] . '/' .  $beta_1['denominator']);
         $beta_y = Fraction::fromString($beta_2['numerator'] . '/' .  $beta_2['denominator']);
+        $gamma2_y = $this->calcGamma2Y($alpha_x, $alpha_y, $beta_x, $beta_y);
         $gamma1_x = $this->calcGamma1X($alpha_x, $alpha_y, $beta_x, $beta_y);
         return [
             'render_params' => [
+                [
+                    'title' => 'gamma2',
+                    'display_text' => sprintf(self::DISPLAY_FORMAT, '0', $gamma2_y->__toString()),
+                    'x' => 0,
+                    'y' => $gamma2_y->toFloat(),
+                ],
                 [
                     'title' => 'beta',
                     'display_text' => sprintf(self::DISPLAY_FORMAT, $beta_x->__toString(), $beta_y->__toString()),
@@ -65,6 +72,24 @@ class Nash
     private function calcGamma1X(Fraction $alpha_x, Fraction $alpha_y, Fraction $beta_x, Fraction $beta_y): Fraction
     {
         $denominator = $beta_y->subtract($alpha_y);
+        $numerator_1 = $alpha_x->multiply($beta_y);
+        $numerator_2 = $alpha_y->multiply($beta_x);
+        $numerator = $numerator_1->subtract($numerator_2);
+        return $numerator->divide($denominator);
+    }
+
+    /**
+     * ガンマ2のY点を計算する
+     * @param Fraction $alpha_x
+     * @param Fraction $alpha_y
+     * @param Fraction $beta_x
+     * @param Fraction $beta_y
+     * @return Fraction
+     * @throws \Exception
+     */
+    private function calcGamma2Y(Fraction $alpha_x, Fraction $alpha_y, Fraction $beta_x, Fraction $beta_y): Fraction
+    {
+        $denominator = $alpha_x->subtract($beta_x);
         $numerator_1 = $alpha_x->multiply($beta_y);
         $numerator_2 = $alpha_y->multiply($beta_x);
         $numerator = $numerator_1->subtract($numerator_2);
